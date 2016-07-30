280d53eb07e959dfe2d83b0a86f318e83b1df457
eddieozzie <eddieozzie@gmail.com>
2016-07-30 14:13:58 -0500

    Adds support for checking guesses, creating board, and type checking

--- ruby/game.rb
+++ ruby/game.rb
@@ -17,21 +17,89 @@ class Game
 	attr_reader :word, :board, :guess_count, :game_over
 
 	def initialize(word)
-		@word = word
+		@word = word.split("")
 		@guess_count = word.length
 		@game_over = false
+	
+
+		create_board
 	end
 
 	def create_board
+		@board = Array.new(@word)
+		@board.map! do |c| 
+			if is_letter? c 
+				"_ "
+			else 
+				" "
+			end
+		end 
 	end
 
-	def ask_for_guess
+	def fill_board(character)
+		@word.length.times do |index|
+
+			if @word[index] == character
+				@board[index] = character
+			end
+
+		end
 	end
 
-	def check_guess
+
+	def print_board
+		print @board.join("")
+	end
+
+	def make_guess(guess)
+		if check_guess(guess)
+			puts "Nice work! The letter #{guess} is here!"
+			fill_board(guess)
+
+		else
+			puts "Sorry, the letter #{guess} is not here!"
+		end
+
+		@guess_count -= 1
+	end
+
+	def check_guess(guess)
+		if @word.include? guess
+			true
+		else
+			false
+		end
 	end
 
 	def update_board
 	end
 
+	def game_over?
+		return true if @guess_count == 0 || won?
+		false
+	end
+
+	def won?
+		@game_over = true
+		return true if !@board.include? "_ "
+	end
+
+	def is_letter?(char)
+  		char =~ /[[:alpha:]]/
+	end
+
 end
+
+puts "Player 1, please enter your word: "
+game = Game.new(gets.chomp)
+
+until game.game_over? do
+	puts game.print_board
+	puts "Player 2, please enter your guess: "
+	game.make_guess(gets.chomp)
+end
+
+if game.won?
+
+
+puts "\nThanks for playing! The phrase was '#{game.word.join}' and you had #{game.guess_count} guesses remaining"
